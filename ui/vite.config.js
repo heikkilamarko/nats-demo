79c648dff16e1188631d0eda8ec854acb4af8d1a
Marko Heikkilä <heikkila_marko@hotmail.com>
2024-10-28 18:17:56 +0200

    Improve local ui dev experience.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		strictPort: true,
+		proxy: {
+			'/ws': {
+				target: 'ws://localhost:8080'
+			},
+			'/nats': {
+				target: 'http://localhost:8080'
+			}
+		}
+	}
 });
